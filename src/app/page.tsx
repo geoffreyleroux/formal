f46c4bf7c92e9f9aaafd12862fd27eb93fe82fbb
Geoffrey Leroux <jojo45452@gmail.com>
2024-05-04 19:34:40 +0200

    plop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
-    document.addEventListener("popstate", () => {
+    window.addEventListener("popstate", () => {
       window.alert("in");
     });
   }, []);
